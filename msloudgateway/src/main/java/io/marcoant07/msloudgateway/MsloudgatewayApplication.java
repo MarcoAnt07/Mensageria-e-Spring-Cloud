afe48e5b22fab82b5d43730193289c74d133384c
Marco Antonio <marcorodoi64@gmail.com>
2024-12-13 18:45:01 -0300

    Configuração Cloud Gateway

---
+++ msloudgateway/src/main/java/io/marcoant07/msloudgateway/MsloudgatewayApplication.java
@@ -0,0 +1,26 @@
+package io.marcoant07.msloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MsloudgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsloudgatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder){
+		return builder
+				.routes()
+				.route(r -> r.path("/cliente?**").uri("lb://msclientes"))
+				.build();
+	}
+
+}
---
+++ msloudgateway/src/test/java/io/marcoant07/msloudgateway/MsloudgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.marcoant07.msloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsloudgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
